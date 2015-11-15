28739e314e1c4ebed86831e3801d64c40a90c6d6
Dale Hamel <dale.hamel@shopify.com>
2015-11-15 18:23:20 -0500

    Rename installer to lxb-live

--- profiles/ubuntu/10_init_root.sh
+++ profiles/ubuntu/10_init_root.sh
@@ -6,6 +6,39 @@ deb http://archive.ubuntu.com/ubuntu trusty-updates main
 EOF
 }
 
+# Set up ttyS1 service so kernel will respond to IPMI
+_serial_console_service()
+{
+  cat > $CONTAINER_HOME/$CONTAINER_NAME/rootfs/etc/init/ttyS1.conf <<EOF
+# ttyS1 - getty
+#
+# This service maintains a getty on ttyS1 from the point the system is
+# started until it is shut down again.
+
+start on stopped rc or RUNLEVEL=[12345]
+stop on runlevel [!12345]
+
+respawn
+
+# Respawn every 10 minutes so we can verify the console is active
+exec /sbin/getty -L 115200 ttyS1 vt102 -t 600
+EOF
+}
+
+_dhcp_interfaces()
+{
+  cat > $CONTAINER_HOME/$CONTAINER_NAME/rootfs/etc/network/interfaces <<EOF
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+
+auto eth1
+iface eth1 inet dhcp
+EOF
+}
+
 # Set it up from scratch
 init_root()
 {
@@ -17,4 +50,5 @@ init_root()
   [ -n "${GLOBAL_BLACKLIST_PACKAGES}${BLACKLIST_PACKAGES}" ] && PACKAGE_ARGS="${PACKAGE_ARGS} --exclude=$(join , ${GLOBAL_BLACKLIST_PACKAGES} ${BLACKLIST_PACKAGES})"
   debootstrap $([ -n "$DEBUG" ] && echo "--keep-debootstrap-dir" ) $PACKAGE_ARGS --variant=minbase --components main,universe --arch amd64 $DISTRO $CONTAINER_HOME/$CONTAINER_NAME/rootfs $MIRROR
   _apt_sources
+  _serial_console_service
 }
